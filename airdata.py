a21ace9bd0e4365d336e0198304ccfa967d0b427
gujingying <91874833+gujingying@users.noreply.github.com>
2022-02-05 00:43:13 +0000

    5/2 update timefilter

--- airdata.py
+++ airdata.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-
+import numpy as np
 import pandas as pd
-
+from datetime import datetime
 
 class RecyclingData:
     """Class for retrieving and structuring the data.
@@ -23,14 +23,17 @@ class RecyclingData:
         #self.recycling = pd.read_csv('data/household_recycling.csv')
         self.area_list = self.recycling["location_x"].unique().tolist()
 
-    def process_data_for_area(self, area):
+    def process_data_for_area(self, area,start_date,end_date):
+        self.recycling['utc'] = pd.to_datetime(self.recycling['utc'])
+        mask_1 = (self.recycling['utc']> start_date) & (self.recycling['utc'] <= end_date) & (self.recycling['location_x'] == 'London')
+        mask_2 = (self.recycling['utc']> start_date) & (self.recycling['utc'] <= end_date) & (self.recycling['location_x'] == area)
         # Data for England
-        self.recycling_eng = self.recycling.loc[self.recycling['location_x'] == 'London']
+        self.recycling_eng = self.recycling.loc[mask_1]
         #by_yr_e = self.recycling_eng.sort_values('utc', ascending=False)
         #by_yr_e = by_yr_e.reset_index(drop=True)
 
         # Data for the selected area
-        self.recycling_area = self.recycling.loc[self.recycling['location_x'] == area]
+        self.recycling_area = self.recycling.loc[mask_2]
         # Calculate the change from the previous year
         #by_yr = self.recycling_area.sort_values('utc', ascending=False)
         #by_yr = by_yr.reset_index(drop=True)
--- dash_app.py
+++ dash_app.py
@@ -11,15 +11,16 @@ from dash import dcc
 from dash import html
 from dash import Output, Input
 from datetime import date
+import datetime
 from airdata import RecyclingData
 from chart import RecyclingChart
 
 # Prepare the data set
 data = RecyclingData()
 area = ''
-start_date = date(2020, 1, 1).isoformat()
-end_date = date(2020, 1, 31).isoformat()
-data.process_data_for_area(area)
+start = '2021-1-1'
+end = '2021-12-31'
+data.process_data_for_area(area, start, end)
 # Create the figures
 rc = RecyclingChart(data)
 airtype_list = ['PM2.5', 'PM10']
@@ -32,8 +33,6 @@ external_stylesheets = [dbc.themes.MINTY]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame see https://plotly.com/python/px-arguments/ for more options
-
-
 app.layout = dbc.Container(fluid=True, children=[
     html.Br(),
     # First row here
@@ -47,26 +46,14 @@ app.layout = dbc.Container(fluid=True, children=[
     dbc.Row([
         # This is for the London area selector and the statistics panel.
         dbc.Col(width=3, children=[
-            html.H6('Select Start Date'),
-                dcc.DatePickerSingle(
-             id='my_date_picker_single_start',
+            html.H5('Select Period'),
+            dcc.DatePickerRange(
+             id='my-date-picker-range',
              min_date_allowed=date(2021, 1, 1),
              max_date_allowed=date(2021, 12, 31),
              initial_visible_month=date(2021, 1, 1),
-             date=date(2021, 1, 1)),
-             html.Div(id='output-container-date-picker-single_start'),
-            html.H6('Select End Date'),
-            dcc.DatePickerSingle(
-             id='my_date_picker_single_end',
-             min_date_allowed=date(2021, 1, 1),
-             max_date_allowed=date(2021, 12, 31),
-             initial_visible_month=date(2021, 1, 1),
-             date=date(2021, 1, 1)),
-             html.Div(id='output-container-date-picker-single_end'),
-        ]),
-
-        # Add the second column here. This is for the figure.
-        dbc.Col(width=9, children=[
+             end_date=date(2021, 12, 31)
+            ),
             html.H5("Select Area"),
             dcc.Dropdown(id="area-select",
                          options=[{"label": x, "value": x}
@@ -78,20 +65,34 @@ app.layout = dbc.Container(fluid=True, children=[
                                   for x in airtype_list],
                 value="PM2.5"),
         ]),
+
+        # Add the second column here. This is for the figure.
+        dbc.Col(width=9, children=[
         dcc.Graph(id='recycle-chart', figure=fig_rc)
+        ]),
     ])
 ])
 
-
 @ app.callback(
     Output("recycle-chart", "figure"),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date'),
     [Input("area-select", "value")],
-    [Input("matter-select", "value")])
-def render_output_panel(area_select, matter_select):
-    data.process_data_for_area(
-        area_select)
-    fig_rc=rc.create_chart(area_select, matter_select)
+    [Input("matter-select", "value")],
+)
+def update_output(start_date, end_date, area_select, matter_select):
+    if start_date is not None:
+      data.process_data_for_area(
+        area_select, start_date, end_date)
+    fig_rc= rc.create_chart(area_select, matter_select)
     return fig_rc
+
+
+# def render_output_panel(area_select, matter_select,date1,date2):
+#    data.process_data_for_area(
+#        area_select,start,end)
+#    fig_rc=rc.create_chart(area_select, matter_select)
+#    return fig_rc
 
 
 if __name__ == '__main__':
